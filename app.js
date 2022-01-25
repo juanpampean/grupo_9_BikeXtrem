3b6961897438b6870b4582d6e778feb61bb64028
juanpampean <juanfernandezborasio@gmail.com>
2022-01-25 17:53:12 -0300

    #

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, './public')))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './grupo_9_BikeXtrem/views/index.html'))
+    res.sendFile(path.join(__dirname, './views/index.html'))
 })
 
 app.listen(3000, () => {
