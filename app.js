fe33a55a1b7c2ce258c102e0d907ee7372f3cb65
Jonima Nuñez <jonimanunez@gmail.com>
2022-01-31 19:29:41 -0300

    Ruta Carrito

--- app.js
+++ app.js
@@ -14,23 +14,17 @@ app.listen(3000, () => { console.log("servidor corriendo") });
 app.get("/", function(req, res) {
     res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
-
 //Ruta Login
-<<
-<< << < HEAD
 app.get("/Login", function(req, res) {
-            res.sendFile(path.resolve(__dirname, "./views/login.html")); ===
-            === =
-            app.get("/Login", function(req, res) {
-                res.sendFile(path.resolve(__dirname, "./views/login_Registro.html")); >
-                ace >>> >>> 6 ca464c8534bcb596073c4a61f55d4f31178b9cf
-            });
+    res.sendFile(path.resolve(__dirname, "./views/login_Registro.html"));
+});
 
-            //Ruta ProductDetail
-            app.get("/ProductDetail", function(req, res) {
-                res.sendFile(path.resolve(__dirname, "./views/ProductDetail.html"));
-            });
-            //Ruta ProductCart
-            app.get("/ProductCart", function(req, res) {
-                res.sendFile(path.resolve(__dirname, "./views/ProductCart.html"));
-            });
+//Ruta ProductDetail
+app.get("/ProductDetail", function(req, res) {
+    res.sendFile(path.resolve(__dirname, "./views/ProductDetail.html"));
+});
+
+//Ruta ProductCart
+app.get("/ProductCart", function(req, res) {
+    res.sendFile(path.resolve(__dirname, "./views/ProductCart.html"));
+});
