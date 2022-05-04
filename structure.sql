a9cc5ccd954b484b22c0e9533d59ddf1aea30f5b
juanpampean <66573948+juanpampean@users.noreply.github.com>
2022-05-04 15:51:47 -0300

    actualizamos script sql

--- structure.sql
+++ structure.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 01-05-2022 a las 17:45:44
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
+-- Host: 127.0.0.1
+-- Generation Time: May 04, 2022 at 08:49 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,25 +18,22 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `bikextrem`
+-- Database: `bikextrem`
 --
-
-CREATE DATABASE bikextrem;
-USE bikextrem;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `genres`
+-- Table structure for table `genres`
 --
 
 CREATE TABLE `genres` (
   `id` int(11) NOT NULL,
   `nombre_genero` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Géneros';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='GÃ©neros';
 
 --
--- Volcado de datos para la tabla `genres`
+-- Dumping data for table `genres`
 --
 
 INSERT INTO `genres` (`id`, `nombre_genero`) VALUES
@@ -47,7 +44,7 @@ INSERT INTO `genres` (`id`, `nombre_genero`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `orders`
+-- Table structure for table `orders`
 --
 
 CREATE TABLE `orders` (
@@ -60,7 +57,7 @@ CREATE TABLE `orders` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Ordenes';
 
 --
--- Volcado de datos para la tabla `orders`
+-- Dumping data for table `orders`
 --
 
 INSERT INTO `orders` (`id`, `fecha_orden`, `usuario_id`, `total_compra`, `direccion_orden`, `status_orden`) VALUES
@@ -69,7 +66,7 @@ INSERT INTO `orders` (`id`, `fecha_orden`, `usuario_id`, `total_compra`, `direcc
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `products`
+-- Table structure for table `products`
 --
 
 CREATE TABLE `products` (
@@ -87,7 +84,7 @@ CREATE TABLE `products` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Productos';
 
 --
--- Volcado de datos para la tabla `products`
+-- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`id`, `SKU`, `nombre`, `precio`, `peso`, `descripcion`, `categoria_id`, `imagen`, `create_date`, `stock`, `proveedor_id`) VALUES
@@ -96,7 +93,7 @@ INSERT INTO `products` (`id`, `SKU`, `nombre`, `precio`, `peso`, `descripcion`, 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `products_orders`
+-- Table structure for table `products_orders`
 --
 
 CREATE TABLE `products_orders` (
@@ -109,7 +106,7 @@ CREATE TABLE `products_orders` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Ordenes de Producto';
 
 --
--- Volcado de datos para la tabla `products_orders`
+-- Dumping data for table `products_orders`
 --
 
 INSERT INTO `products_orders` (`id`, `orden_id`, `producto_id`, `precio`, `cantidad`, `subtotal_compra`) VALUES
@@ -118,7 +115,7 @@ INSERT INTO `products_orders` (`id`, `orden_id`, `producto_id`, `precio`, `canti
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `product_category`
+-- Table structure for table `product_category`
 --
 
 CREATE TABLE `product_category` (
@@ -127,7 +124,7 @@ CREATE TABLE `product_category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Categoria del Producto';
 
 --
--- Volcado de datos para la tabla `product_category`
+-- Dumping data for table `product_category`
 --
 
 INSERT INTO `product_category` (`id`, `nombre`) VALUES
@@ -139,7 +136,7 @@ INSERT INTO `product_category` (`id`, `nombre`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `suppliers`
+-- Table structure for table `suppliers`
 --
 
 CREATE TABLE `suppliers` (
@@ -150,7 +147,7 @@ CREATE TABLE `suppliers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Proveedores';
 
 --
--- Volcado de datos para la tabla `suppliers`
+-- Dumping data for table `suppliers`
 --
 
 INSERT INTO `suppliers` (`id`, `nombre`, `telefono`, `mail`) VALUES
@@ -160,7 +157,7 @@ INSERT INTO `suppliers` (`id`, `nombre`, `telefono`, `mail`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -168,42 +165,47 @@ CREATE TABLE `users` (
   `nombre` varchar(45) DEFAULT NULL,
   `apellido` varchar(45) DEFAULT NULL,
   `mail` varchar(100) DEFAULT NULL,
-  `contraseña` varchar(100) DEFAULT NULL,
+  `contraseÃ±a` varchar(100) DEFAULT NULL,
   `telefono` varchar(100) DEFAULT NULL,
   `domicilio_entrega` varchar(255) DEFAULT NULL,
   `avatar` longblob DEFAULT NULL,
-  `genero_id` int(11) NOT NULL,
+  `genero_id` int(11) DEFAULT NULL,
   `codigo_postal` varchar(11) DEFAULT NULL,
-  `fecha_nacimiento` datetime DEFAULT NULL
+  `fecha_nacimiento` datetime DEFAULT NULL,
+  `ciudad` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Usuarios';
 
 --
--- Volcado de datos para la tabla `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `nombre`, `apellido`, `mail`, `contraseña`, `telefono`, `domicilio_entrega`, `avatar`, `genero_id`, `codigo_postal`, `fecha_nacimiento`) VALUES
-(1, 'Juan', 'Garcia', 'juangarcia@gmail.com', '$10$HObbvMbGTFxwAWhiF2WL1OBG11gLAp9NegBKWPhxbnffz96Bx5t16', NULL, 'General Pico, La Pampa', NULL, 2, NULL, '2022-04-14 11:10:01'),
-(2, 'Kerby', 'Tovar', 'kerbytovar@gmail.com', '$2a$10$3cWTqBRxT/3hd7gguFhRm.aVO.H9FXiBXysNhSVTXYpimUWfUYuVu', '+5491123456789', 'Villa Urquiza, CABA', NULL, 1, '1431', '2022-05-01 16:10:01');
+INSERT INTO `users` (`id`, `nombre`, `apellido`, `mail`, `contraseÃ±a`, `telefono`, `domicilio_entrega`, `avatar`, `genero_id`, `codigo_postal`, `fecha_nacimiento`, `ciudad`) VALUES
+(1, 'Juan', 'Garcia', 'juangarcia@gmail.com', '$10$HObbvMbGTFxwAWhiF2WL1OBG11gLAp9NegBKWPhxbnffz96Bx5t16', NULL, 'General Pico, La Pampa', NULL, 2, NULL, '2022-04-14 11:10:01', NULL),
+(2, 'Kerby', 'Tovar', 'kerbytovar@gmail.com', '$2a$10$3cWTqBRxT/3hd7gguFhRm.aVO.H9FXiBXysNhSVTXYpimUWfUYuVu', '+5491123456789', 'Villa Urquiza, CABA', NULL, 1, '1431', '2022-05-01 16:10:01', NULL),
+(3, 'Pedro', 'Gomez', 'pedro14@gmail.com', '$2a$10$.jRYk4pvCTTVJlltXOKcX.KGJDLayE5B31Zn1ENfpIJfrlh1V8iOG', '3516311211', 'Belgrano 45', NULL, 2, '23222', '2022-05-28 00:00:00', 'SUNCHALES'),
+(4, 'Augusto', 'Garcia', 'juanfernandezborasio@gmail.com', '$2a$10$/Iv.bwgKw9lLgvMRBBjrqOOROGEBAUrqyj9/6NPYLUL1SWawmD7q.', '3516311212', 'Zapiola 96', NULL, 2, '2322', '1991-09-04 00:00:00', 'SUNCHALES'),
+(5, 'Carlos', 'Garcia', 'manuel14@gmail.com', '$2a$10$NnErKEPk7kHO7/24Oi00JOeG44eKWBmviXNiS5FIG.Rj3b52mh1JG', '14423042', 'Velez 1342', NULL, 2, '5000', '1932-02-04 00:00:00', 'Cordoba'),
+(6, 'Marcos', 'Gutierrez', 'manuel@gmail.com', '$2a$10$F2cxdXfAXNHyOT3bf96.P.5AV5wN2cZtpppL7S0fIlpNy0yu9F4ya', '5555555', 'Ruta 5 Km 145', NULL, 2, '4050', '1988-01-04 00:00:00', 'Buenos Aires');
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `genres`
+-- Indexes for table `genres`
 --
 ALTER TABLE `genres`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `orders`
+-- Indexes for table `orders`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
   ADD KEY `usuario_id` (`usuario_id`);
 
 --
--- Indices de la tabla `products`
+-- Indexes for table `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
@@ -211,7 +213,7 @@ ALTER TABLE `products`
   ADD KEY `proveedor_id` (`proveedor_id`);
 
 --
--- Indices de la tabla `products_orders`
+-- Indexes for table `products_orders`
 --
 ALTER TABLE `products_orders`
   ADD PRIMARY KEY (`id`),
@@ -219,96 +221,96 @@ ALTER TABLE `products_orders`
   ADD KEY `producto_id` (`producto_id`);
 
 --
--- Indices de la tabla `product_category`
+-- Indexes for table `product_category`
 --
 ALTER TABLE `product_category`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `suppliers`
+-- Indexes for table `suppliers`
 --
 ALTER TABLE `suppliers`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD KEY `genero_id` (`genero_id`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `genres`
+-- AUTO_INCREMENT for table `genres`
 --
 ALTER TABLE `genres`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT de la tabla `orders`
+-- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `products`
+-- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT de la tabla `products_orders`
+-- AUTO_INCREMENT for table `products_orders`
 --
 ALTER TABLE `products_orders`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de la tabla `product_category`
+-- AUTO_INCREMENT for table `product_category`
 --
 ALTER TABLE `product_category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT de la tabla `suppliers`
+-- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT de la tabla `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `orders`
+-- Constraints for table `orders`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `usuario_id` FOREIGN KEY (`usuario_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `products`
+-- Constraints for table `products`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `categoria_id` FOREIGN KEY (`categoria_id`) REFERENCES `product_category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `proveedor_id` FOREIGN KEY (`proveedor_id`) REFERENCES `suppliers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `products_orders`
+-- Constraints for table `products_orders`
 --
 ALTER TABLE `products_orders`
   ADD CONSTRAINT `orden_id` FOREIGN KEY (`orden_id`) REFERENCES `orders` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `producto_id` FOREIGN KEY (`producto_id`) REFERENCES `products` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `users`
+-- Constraints for table `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `genero_id` FOREIGN KEY (`genero_id`) REFERENCES `genres` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
