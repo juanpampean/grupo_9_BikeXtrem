6e6d2125db5199c9d648bf0e1430afe882d20376
juanpampean <66573948+juanpampean@users.noreply.github.com>
2022-05-22 12:19:55 -0300

    Validación Front End para nombre y descripción de producto al momento de editar listo

--- public/js/prodUpdateValidation.js
+++ public/js/prodUpdateValidation.js
@@ -1,34 +1,39 @@
-const { format } = require("bytes");
 
 window.onload = function(){
     let titulo = document.querySelector("h2");
     
     titulo.style.color="red"
 
-    nombre.focus()
+    let nombre = document.querySelector("#nombre");
+    let form = document.querySelector(".form")
+    let descripcion = document.querySelector("#descripcion")
+    nombre.focus();
 
-    form.addEventListener("submit",function(e){
+    form.addEventListener("submit", function(e){
         let errores = [];
         if (nombre.value == ""){
-            errores.push('El campo nombre está vacío')
-        }
+            errores.push("El campo de título está vacío")
+        } else{}
+        if (nombre.value.length <5){
+            errores.push("El campo de nombre debe tener al menos 5 carácteres")
+        } else{}
+        if (descripcion.value.length <20){
+            errores.push("El campo Descripción debe tener al menos 20 carácteres")
+        } else{} 
+
+
 
         if (errores.length > 0){
             e.preventDefault();
             let ulErrores = document.querySelector(".errores");
-            console.log(ulErrores)
             ulErrores.innerHTML = "";
-            console.log(ulErrores)
             errores.forEach(function(error){
                 ulErrores.innerHTML += "<li>" + error +  "</li>"
             })
             console.log(ulErrores)
         }else{
-            alert("La pelicula se guardo satisfactoriamente")
-
+            alert("El producto se editó satisfactoriamente")
         }
-    })
-
     
-
+    })
 }
