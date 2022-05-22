f8aba8dd8fcd168e3b5c130ba3fbc9ca618e9863
juanpampean <66573948+juanpampean@users.noreply.github.com>
2022-05-22 11:56:12 -0300

    saqué los required de formulario de edit

--- public/js/prodUpdateValidation.js
+++ public/js/prodUpdateValidation.js
@@ -1,7 +1,34 @@
+const { format } = require("bytes");
+
 window.onload = function(){
     let titulo = document.querySelector("h2");
     
-    titulo.innerHTML += " Vinculado"
+    titulo.style.color="red"
+
+    nombre.focus()
+
+    form.addEventListener("submit",function(e){
+        let errores = [];
+        if (nombre.value == ""){
+            errores.push('El campo nombre está vacío')
+        }
+
+        if (errores.length > 0){
+            e.preventDefault();
+            let ulErrores = document.querySelector(".errores");
+            console.log(ulErrores)
+            ulErrores.innerHTML = "";
+            console.log(ulErrores)
+            errores.forEach(function(error){
+                ulErrores.innerHTML += "<li>" + error +  "</li>"
+            })
+            console.log(ulErrores)
+        }else{
+            alert("La pelicula se guardo satisfactoriamente")
+
+        }
+    })
+
     
 
 }
