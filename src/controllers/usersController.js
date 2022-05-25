50aaa1f8e3c9fdf42024b4268b9056adecd28234
juanpampean <66573948+juanpampean@users.noreply.github.com>
2022-05-25 19:15:16 -0300

    avanzando un poco mas

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -51,7 +51,7 @@ module.exports = {
             domicilio_entrega:req.body.domicilio_entrega,
             codigo_postal:req.body.codigo_postal,
             fecha_nacimiento:req.body.cumpleaños,
-            avatar:req.file ? req.file.filename : req.body.avatar,
+            avatar: req.file ? req.file.filename : req.userLog.avatar,
             genero_id:req.body.genre_id,
         }).then(function(){
             return res.redirect('/users/Login')
