7cc9dbdda7be65272a3fa0d9bbf4fb699a18d59f
Jonima Nuñez <jonimanunez@gmail.com>
2022-05-25 22:48:57 -0300

    login validado

--- public/js/loginValidationFront.js
+++ public/js/loginValidationFront.js
@@ -1,39 +1,36 @@
 window.onload = function() {
-    let ingreso = document.querySelector("h4");
-    ingreso.style.color = "blue"
-
     let boton = document.querySelector(".form ");
-    boton.style.backgroundColor = "LightBlue";
 
+    let mail = document.querySelector("#email");
+    let clave = document.querySelector("#clave")
+    mail.focus();
 
-    //Evento para el boton al hacer submit
     boton.addEventListener("submit", function(e) {
         let errores = [];
-        //Valido que el correo no esté vacío
-        let correo = document.querySelector("#email");
-        if (correo.value == "") {
+        if (mail.value == "") {
             errores.push("El campo de email está vacío")
+        } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(mail)) {
+            alert("La dirección de email " + mail + " es correcta.");
         } else {
-
-        };
-
-        //Validaciones Campo Contraseña
-        let clave = document.querySelector("#clave");
-        if (clave.value.length < 8) {
-            errores.push("El campo contraseña debe tener al menos 8 carácteres")
-        } else {};
-    });
-
-    //Mostrando Errores
-    if (errores.length > 0) {
-        e.preventDefault();
-        let ulErrores = document.querySelector('.errores');
-        for (let i = 0; i < errores.length; i++) {
-            ulErrores.innerHTML += '<li>' + errores[i] + '</li>';
+            alert("La dirección de email es incorrecta.");
         }
-        ulErrores.style.color = "red"
-        console.log(ulErrores);
-    } else {
-        alert('Usuario Logueado Satisfactoriamente');
-    }
-};
+        if (clave.value.length < 8) {
+            errores.push("El campo Contraseña debe tener al menos 8 carácteres")
+        } else {}
+
+
+        if (errores.length > 0) {
+            e.preventDefault();
+            let ulErrores = document.querySelector(".errores");
+            ulErrores.innerHTML = "";
+            errores.forEach(function(error) {
+                ulErrores.innerHTML += "<li>" + error + "</li>"
+            })
+            console.log(ulErrores)
+            ulErrores.style.color = "red";
+        } else {
+            alert("Usuario Logueado Satisfactoriamente")
+        }
+
+    })
+}
