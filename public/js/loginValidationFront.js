551b9fb1667d1961aeae08f8ee54301db9dc7bc2
Jonima Nuñez <jonimanunez@gmail.com>
2022-05-25 21:18:19 -0300

    login validado

--- public/js/loginValidationFront.js
+++ public/js/loginValidationFront.js
@@ -1,29 +1,39 @@
 window.onload = function() {
-
     let ingreso = document.querySelector("h4");
     ingreso.style.color = "blue"
 
-    let correo = document.querySelector("#email");
-    let clave = document.querySelector("#clave");
     let boton = document.querySelector(".form ");
+    boton.style.backgroundColor = "LightBlue";
 
-    let errores = [];
 
-    boton.style.backgroundColor = "Red";
-
+    //Evento para el boton al hacer submit
     boton.addEventListener("submit", function(e) {
-
+        let errores = [];
+        //Valido que el correo no esté vacío
+        let correo = document.querySelector("#email");
         if (correo.value == "") {
             errores.push("El campo de email está vacío")
-        } else {}
+        } else {
 
-        if (clave.value.length < 4) {
-            errores.push("El campo contraseña debe tener al menos 4 carácteres")
-        } else {}
-        if (errores.length > 0) {
-            e.preventDefault();
-            alert("Ingresa email y Contraseña");
+        };
+
+        //Validaciones Campo Contraseña
+        let clave = document.querySelector("#clave");
+        if (clave.value.length < 8) {
+            errores.push("El campo contraseña debe tener al menos 8 carácteres")
+        } else {};
+    });
+
+    //Mostrando Errores
+    if (errores.length > 0) {
+        e.preventDefault();
+        let ulErrores = document.querySelector('.errores');
+        for (let i = 0; i < errores.length; i++) {
+            ulErrores.innerHTML += '<li>' + errores[i] + '</li>';
         }
-
-    })
-}
+        ulErrores.style.color = "red"
+        console.log(ulErrores);
+    } else {
+        alert('Usuario Logueado Satisfactoriamente');
+    }
+};
