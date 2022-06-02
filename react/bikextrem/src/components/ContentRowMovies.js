1af60a5c75e25ab6a398d360ee41f69f561a289f
juanpampean <66573948+juanpampean@users.noreply.github.com>
2022-06-02 14:07:50 -0300

    avances sobre totales

--- react/bikextrem/src/components/ContentRowMovies.js
+++ react/bikextrem/src/components/ContentRowMovies.js
@@ -1,59 +1,81 @@
-import React from 'react'
+
+import React, { useState, useEffect } from "react";
+import SmallCard from "./SmallCard";
 
 function ContentRowMovies() {
+    let [products, setProducts] = useState("No dispone");
+    let [users, setUsers] = useState("Sin usuarios");
+    let [suppliers, setMarcas] = useState("No dispone")
+    let [categories, setCategory] = useState("No dispone")
+
+    let datos = [
+        {
+            titulo: "Products in Data Base",
+            cifra: products,
+            colorBorder: "primary",
+            icono: "fa-clipboard-list"
+        },
+
+        {
+            titulo: "Users quantity",
+            cifra: users,
+            colorBorder: "warning",
+            icono: "fa-user-check"
+        },
+        {
+            titulo: "Total Suppliers",
+            cifra: suppliers,
+            colorBorder: "success",
+            icono: "fa-dollar-sign"
+        },
+        {
+            titulo: "Total Categories",
+            cifra: categories,
+            colorBorder: "warning",
+            icono: "fa-user-check"
+        }
+    ]
+    
+    useEffect(() => {
+        fetch("http://localhost:3000/api/products")
+        .then(resultado => resultado.json())
+        .then(dataProduct => {
+            setProducts(dataProduct.meta.total)
+        })
+
+        fetch("http://localhost:3000/api/users")
+        .then(resultado => resultado.json())
+        .then(dataUser => {
+            setUsers(dataUser.meta.total)
+        })
+
+        fetch("http://localhost:3000/api/suppliers")
+        .then(resultado => resultado.json())
+        .then(dataSupp => {
+            setMarcas(dataSupp.meta.total_suppliers)
+        })
+
+        fetch("http://localhost:3000/api/categories")
+        .then(resultado => resultado.json())
+        .then(dataCat => {
+            setCategory(dataCat.meta.total_categories)
+        })
+
+   }, [])
+
+
     return (					
-    <div className="row">
-    <div className="col-md-4 mb-4">
-        <div className="card border-left-primary shadow h-100 py-2">
-            <div className="card-body">
-                <div className="row no-gutters align-items-center">
-                    <div className="col mr-2">
-                        <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Movies in Data Base</div>
-                        <div className="h5 mb-0 font-weight-bold text-gray-800">21</div>
-                    </div>
-                    <div className="col-auto">
-                        <i className="fas fa-film fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div className="row">
+        {datos.map((dato, i) => 
+            <SmallCard key={i} 
+                titulo={dato.titulo} 
+                cifra={dato.cifra}
+                colorBorder={dato.colorBorder}
+                icono={dato.icono}
+            /> 
+        )}
     </div>
-
-
-    <div className="col-md-4 mb-4">
-        <div className="card border-left-success shadow h-100 py-2">
-            <div className="card-body">
-                <div className="row no-gutters align-items-center">
-                    <div className="col mr-2">
-                        <div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Total awards</div>
-                        <div className="h5 mb-0 font-weight-bold text-gray-800">79</div>
-                    </div>
-                    <div className="col-auto">
-                        <i className="fas fa-award fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div className="col-md-4 mb-4">
-        <div className="card border-left-warning shadow h-100 py-2">
-            <div className="card-body">
-                <div className="row no-gutters align-items-center">
-                    <div className="col mr-2">
-                        <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Actors quantity
-                        </div>
-                        <div className="h5 mb-0 font-weight-bold text-gray-800">49</div>
-                    </div>
-                    <div className="col-auto">
-                        <i className="fas fa-user fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>)
+    )
 }
 
 export default ContentRowMovies
